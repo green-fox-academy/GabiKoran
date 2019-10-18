9df632eed2c0657b3f3d59f914646aa7d847d438
GabiKoran <korangabriella@gmail.com>
2019-10-18 12:18:56 +0200

    workshop first part

---
+++ week-03/day-3/src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Viki!");
+        System.out.println("Hello Eszter!");
+        System.out.println("Hello Hajni!");
+    }
+}
+
---
+++ week-03/day-3/src/IntroduceYourself.java
@@ -0,0 +1,19 @@
+public class IntroduceYourself {
+        public static void main(String[] args) {
+                System.out.println("Gabi Korán");
+                System.out.println(40);
+                System.out.println(1.66);
+
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+        }
+
+}
