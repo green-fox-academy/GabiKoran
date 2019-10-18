368a441770876248ff531cbeba16697a169eb8ac
GabiKoran <korangabriella@gmail.com>
2019-10-18 12:41:29 +0200

    codes beautified

--- week-03/day-3/src/IntroduceYourself.java
+++ week-03/day-3/src/IntroduceYourself.java
@@ -1,19 +1,20 @@
 public class IntroduceYourself {
         public static void main(String[] args) {
+                // Write a program that prints a few details to the terminal window about you
+                // It should print each detail to a new line.
+                //  - Your name
                 System.out.println("Gabi Korán");
+
+                //  - Your age
                 System.out.println(40);
+
+                //  - Your height in meters (as a decimal fraction)
                 System.out.println(1.66);
 
-            // Write a program that prints a few details to the terminal window about you
-            // It should print each detail to a new line.
-            //  - Your name
-            //  - Your age
-            //  - Your height in meters (as a decimal fraction)
-            //
-            //  Example output:
-            //  John Doe
-            //  31
-            //  1.87
+                //  Example output:
+                //  John Doe
+                //  31
+                //  1.87
         }
 
 }
