305edfd9d14b3a962f6548d7ae578e259397d3cd
GabiKoran <korangabriella@gmail.com>
2019-11-14 14:18:25 +0100

    with function

--- 071_FileIO_FileManipulation_HandlingExceptions/src/_08ReversedOrder.java
+++ 071_FileIO_FileManipulation_HandlingExceptions/src/_08ReversedOrder.java
@@ -8,19 +8,22 @@ public class _08ReversedOrder {
 
     public static void main(String[] args) {
         // Create a method that decrypts reversed-order.txt
-        Path filePath = Paths.get("assets/reversed-order.txt");
+        Path filePath = Paths.get("assets/reversed-oredr.txt");
         List<String> origiText = new ArrayList<>();
-//        List<String> decryptedText = new ArrayList<>();
-        String decryptedString = "";
+
         try {
             origiText = Files.readAllLines(filePath);
         } catch (Exception ex) {
             System.out.println("valami gigszi van, de legalább nem errorozik");
         }
-        for (int i = origiText.size(); i > 0; i--) {
-//            decryptedText.add(origiText.get(i));
-            decryptedString += origiText.get(i);
+        System.out.println(reverseText(origiText));
+    }
+
+    public static String reverseText (List<String> origiText) {
+        String decryptedString = "";
+        for (int i = (origiText.size() -1); i > 0; i--) {
+            decryptedString += origiText.get(i) + "\n";
         }
-        System.out.println(decryptedString);
+        return decryptedString;
     }
 }
