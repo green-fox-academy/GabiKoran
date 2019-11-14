231dfbeb2a0f02617f49c44804027864e1b5d433
GabiKoran <korangabriella@gmail.com>
2019-11-14 11:15:37 +0100

    logs done

--- 071_FileIO_FileManipulation_HandlingExceptions/src/_09Logs.java
+++ 071_FileIO_FileManipulation_HandlingExceptions/src/_09Logs.java
@@ -1,8 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _09Logs {
     public static void main(String[] args) {
@@ -12,42 +11,39 @@ public class _09Logs {
         // Write a function that returns the GET / POST request ratio.
         Path filePath = Paths.get("assets/log.txt");
         List<String> logs = new ArrayList<>();
-        int flag = 0;
-        int count = 0;
+
         try {
             logs = Files.readAllLines(filePath);
         } catch (Exception ex) {
             System.out.println("valami gigszi van, de legalább nem errorozik");
         }
-        String[] IPAdresses = new String[logs.size()];
+        ArrayList<String> IPAdresses = new ArrayList<>();
         for (int i = 0; i < logs.size(); i++) {
-            IPAdresses[i] = "";
-            for (int j = 27; j < 38; j++) {
-                System.out.print(logs.get(i).charAt(j));    // kinyomtatja
-                IPAdresses[i] += logs.get(i).charAt(j);     // ugyanazt beleteszi ez IPAdresses tömbbe
+            String[] lineParts = logs.get(i).split(" ");
+                IPAdresses.add(lineParts[8]);
+        }
+        Set<String> uniqueIPs = new HashSet<String>(IPAdresses);
+        int numberOfIPs = uniqueIPs.size();
+        System.out.println(numberOfIPs);
+        String[] uniqueIPAdresses = new String[numberOfIPs];
+        System.arraycopy(uniqueIPs.toArray(), 0, uniqueIPAdresses, 0, numberOfIPs);
+        System.out.println(Arrays.toString(uniqueIPAdresses));
+
+        ArrayList<String> coloumnGetsPosts = new ArrayList<>();
+        for (int i = 0; i < logs.size(); i++) {
+            String[] lineParts = logs.get(i).split(" ");
+            coloumnGetsPosts.add(lineParts[11]);
+        }
+        int sumGets = 0;
+        int sumPosts = 0;
+        for (int i = 0; i < coloumnGetsPosts.size(); i++) {
+            System.out.println(coloumnGetsPosts.get(i));
+            if (coloumnGetsPosts.get(i).equals("GET")) {
+                sumGets ++;
+            } else if (coloumnGetsPosts.get(i).equals("POST")) {
+                sumPosts ++;
             }
-            System.out.println();                           // nyomtatáshoz sortörés
         }
-        for (int i = 0; i < IPAdresses.length; i++) {
-            System.out.println(IPAdresses[i]);
-        }
-//        for (int i = 0; i < IPAdresses.length; i++) {
-//            System.out.println(IPAdresses[i].charAt(4));
-//        }
-//        for (int i = 0; i < IPAdresses.length; i++) {
-//            for (int j = 0; j < IPAdresses.length; j++) {
-//                if (i != j) {
-//                    if (IPAdresses[i] != IPAdresses[j]) {
-//                        flag = 1;
-//                    } else {
-//                        flag = 0;
-//                        break;
-//                    }
-//                }
-//            } if (flag == 1) {
-//                count ++;
-//                System.out.println(IPAdresses[i]);
-//            }
-//        }
+        System.out.println("GET/POST request ratio: " + sumGets + "/" + sumPosts);
     }
 }
