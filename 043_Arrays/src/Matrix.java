9a12a67a14b3d2d52ee206eba11a6e8857c79a77
GabiKoran <korangabriella@gmail.com>
2019-11-22 13:13:31 +0100

    redone with Kond's code

--- 043_Arrays/src/Matrix.java
+++ 043_Arrays/src/Matrix.java
@@ -12,27 +12,28 @@ public class Matrix {
         matrix[3] = new int[]{0, 0, 0, 1};
         //System.out.println(matrix[2][3]);
         */
+        int size = 4;
+        int[][] matrix = createMatrix(size);
+        printMatrix(matrix);
+    }
 
-        int element = 4;
-        int[][] matrix = new int[element][];
-        for (int i = 0; i < element ; i++) {
-            matrix[i] = new int[element];
-            for (int j = 0; j < element; j++) {
-                if (j == i) {
-                    matrix[i][j] = 1;
-                } else {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // - Print this two dimensional array to the output
-
-        for (int i = 0; i < matrix.length ; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+    public static void printMatrix(int[][] matrix) {
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < matrix[rowIndex].length; columnIndex++) {
+                System.out.print(matrix[rowIndex][columnIndex] + " ");
             }
             System.out.println();
         }
     }
+
+    public static int[][] createMatrix(int size) {
+        int[][] matrix = new int[size][size];
+
+        for (int index = 0; index < size; index++) {
+            matrix[index][index] = 1;       // elég csak ezeket 1-re változtatni, mert a többi alapból 0, az az integerek default értéke
+        }
+
+        return matrix;
+    }
+
 }
