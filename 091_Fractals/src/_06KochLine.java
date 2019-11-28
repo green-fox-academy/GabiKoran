f7fb13a142eeda0564148f06a7510c52185b7feb
GabiKoran <korangabriella@gmail.com>
2019-11-28 12:17:14 +0100

    with recursion

--- 091_Fractals/src/_06KochLine.java
+++ 091_Fractals/src/_06KochLine.java
@@ -5,34 +5,29 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class _06KochLine {
-    public static void mainDraw(Graphics graphics){
-        int size = 120;
-        int startX = 50;
-        int startY = 200;
-        int angle = 0;
+    public static void mainDraw(Graphics graphics) {
 
-        drawAKochLine(startX, startY, size, angle, graphics);
+        koch(5, 200, 600, 600, 600, graphics);
     }
 
-    public static void drawAKochLine (int startX, int startY, int size, int angle, Graphics graphics) {
-        graphics.drawLine(startX, startY, startX + (int)(Math.cos(angle)*size), startY + (int)(Math.sin(angle)*size));
-        startX = startX + (int)(Math.cos(angle)*size);
-        startY = startY + (int)(Math.sin(angle)*size);
-        angle -= 1;
-        graphics.drawLine(startX, startY, startX + (int)(Math.cos(angle)*size), startY + (int)(Math.sin(angle)*size));
-        startX = startX + (int)(Math.cos(angle)*size);
-        startY = startY + (int)(Math.sin(angle)*size);
-        angle += 2;
-        graphics.drawLine(startX, startY, startX + (int)(Math.cos(angle)*size), startY + (int)(Math.sin(angle)*size));
-        startX = startX + (int)(Math.cos(angle)*size);
-        startY = startY + (int)(Math.sin(angle)*size);
-        angle -= 1;
-        graphics.drawLine(startX, startY, startX + (int)(Math.cos(angle)*size), startY + (int)(Math.sin(angle)*size));
+    private static void koch(int n, int x1, int y1, int x5, int y5, Graphics graphics) {
+        if (n == 1)
+            graphics.drawLine(x1, y1, x5, y5);
+        else {
+            double gy = Math.sqrt(3) / 6; // gyök 3-t osztom 6-tal
+            int dx = x5 - x1;
+            int dy = y5 - y1;
+            int x2 = x1 + dx / 3;
+            int y2 = y1 + dy / 3;
+            int x3 = (int) ((x1 + x5) / 2 + gy * (y1 - y5));
+            int y3 = (int) ((y1 + y5) / 2 + gy * (x5 - x1));
+            int x4 = x1 + dx * 2 / 3, y4 = y1 + dy * 2 / 3;
+            koch(n - 1, x1, y1, x2, y2, graphics);
+            koch(n - 1, x2, y2, x3, y3, graphics);
+            koch(n - 1, x3, y3, x4, y4, graphics);
+            koch(n - 1, x4, y4, x5, y5, graphics);
+        }
     }
-    public static void drawLine (int startX, int startY, int size, int angle, Graphics graphics) {
-        graphics.drawLine(startX, startY, startX + (int)(Math.cos(angle)*size), startY + (int)(Math.sin(angle)*size));
-    }
-
 
     // Don't touch the code below
     static int WIDTH = 800;
