a2aa04ee881a411f58beaa8572b326e02fff472e
GabiKoran <korangabriella@gmail.com>
2019-12-01 10:10:30 +0100

    removed unnecessary lines

--- 084_Recursion/src/_11MaximumFinder.java
+++ 084_Recursion/src/_11MaximumFinder.java
@@ -6,22 +6,8 @@ import java.util.Arrays;
 
 public class _11MaximumFinder {
     public static void main(String[] args) {
-        //Write a function that finds the largest element of an array using recursion.
         int A[] = {7, 3, 1, 9, 2, 5, 3, 6, 8, 0, 1};
-        System.out.println(A.length);
-        
-        // for ciklussal:
-//        int max = 0;
-//        for (int i = 0; i < A.length; i++) {
-//            if (A[i] > max) {
-//                max = A[i];
-//            }
-//        }
-//        System.out.println(max);
-
-        // with recursion:
         System.out.println(maximumFinder(A));
-
     }
 
     private static int maximumFinder(int A[]) {
@@ -38,7 +24,6 @@ public class _11MaximumFinder {
             B[B.length - 1] = A[A.length - 2];
         }
         A = B;
-        //System.out.println(Arrays.toString(A)); // csak, hogy lássam, mi történik menet közben
         return maximumFinder(A);
     }
 }
