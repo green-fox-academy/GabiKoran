808b815ade3253b4f8de0dc63d347fc4cab60abc
GabiKoran <korangabriella@gmail.com>
2019-12-04 04:21:46 +0100

    calculator with 2 functions & OOP at week-10

---
+++ 053_DataStructures/src/_18Calculator/Input.java
@@ -0,0 +1,34 @@
+package _18Calculator;
+
+public class Input {
+    private String operation;
+    private int operandA;
+    private int operandB;
+
+    public Input() {
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public int getOperandA() {
+        return operandA;
+    }
+
+    public void setOperandA(int operationA) {
+        this.operandA = operationA;
+    }
+
+    public int getOperandB() {
+        return operandB;
+    }
+
+    public void setOperandB(int operandB) {
+        this.operandB = operandB;
+    }
+}
--- 053_DataStructures/src/_18Calculator/_18Calculator.java
+++ 053_DataStructures/src/_18Calculator/_18Calculator.java
@@ -1,32 +1,37 @@
+package _18Calculator;
+
 import java.util.Scanner;
 
 public class _18Calculator {
     public static void main(String... args) {
-        System.out.println("Please type in the expression:");
-        Scanner scanner = new Scanner(System.in);
-        String operation = scanner.next();
-        int operandA = scanner.nextInt();
-        int operandB = scanner.nextInt();
-
-        System.out.println(calculate(operation, operandA, operandB));
+        calculate(getUserInput());
     }
 
-    // sima string a kimenet a getUserInputnál
-    public static int calculate(String operation, int operandA, int operandB) { // classban 3 field a bemenet
-        int result;
-        if (operation.equals("+")) {
-            result = operandA + operandB;
-        } else if (operation.equals("-")) {
-            result = operandA - operandB;
-        } else if (operation.equals("*")) {
-            result = operandA * operandB;
-        } else if (operation.equals("/")) {
-            result = operandA / operandB;
-        } else if (operation.equals("%")) {
-            result = operandA % operandB;
-        } else {
-            result = -9999999;
-        }
-        return result;
+    public static Input getUserInput() {
+        System.out.println("Please type in the expression:");
+        Scanner scanner = new Scanner(System.in);
+        Input input = new Input();
+        input.setOperation(scanner.next());
+        input.setOperandA(scanner.nextInt());
+        input.setOperandB(scanner.nextInt());
+        return input;
+    }
+    
+    public static void calculate(Input input) {
+            int result;
+            if (input.getOperation().equals("+")) {
+                result = input.getOperandA() + input.getOperandB();
+            } else if (input.getOperation().equals("-")) {
+                result = input.getOperandA() - input.getOperandB();
+            } else if (input.getOperation().equals("*")) {
+                result = input.getOperandA() * input.getOperandB();
+            } else if (input.getOperation().equals("/")) {
+                result = input.getOperandA() / input.getOperandB();
+            } else if (input.getOperation().equals("%")) {
+                result = input.getOperandA() % input.getOperandB();
+            } else {
+                result = -99999999; // I don't know how could I catch this exception
+            }
+        System.out.println(result);
     }
 }
