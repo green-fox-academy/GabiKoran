a4864a808f8f9c90d6773e430c9ceca1e842bd09
GabiKoran <korangabriella@gmail.com>
2020-01-10 12:40:04 +0100

    readme wanderer

--- 113_Testing/src/Main.java
+++ 113_Testing/src/Main.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws NegativeIntegerException {
-      Fibonacci fibonacci = new Fibonacci();
-        System.out.println(fibonacci.fibonacci(-1));
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList('a', 'u', 'o', 'e', 'i').contains('a'));
+        Extension extension = new Extension();
+        System.out.println(extension.isVowel('a'));
     }
 }
