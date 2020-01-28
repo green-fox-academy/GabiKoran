8873e6a6963ee120f9e5c34dba1fd6e6497c7cdc
GabiKoran <korangabriella@gmail.com>
2020-01-28 22:15:09 +0100

    workshop 2. part

---
+++ 151_Streams/src/Exercise06.java
@@ -0,0 +1,14 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Exercise06 {
+
+    public static void main(String[] args) {
+        //Write a Stream Expression to find the uppercase characters in a string!
+        String text = "KisEsNagyBetuk";
+        String[] characters = text.split("");
+    }
+}
