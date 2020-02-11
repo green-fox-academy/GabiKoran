ea416bc73812c0d4e589fee6b6184c10b8bb63ff
GabiKoran <korangabriella@gmail.com>
2020-02-11 11:06:20 +0100

    rearrange file and directories

--- 171_Spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ 171_Spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,18 +1,21 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWebController {
 
     @GetMapping("/web/greeting")
-    @ResponseBody
     public String greeting(@RequestParam(value = "name", required = false) String name, Model model) {
         if (name == null) {
             name = "World";
         }
-        model.addAttribute("name", " World");
-        return "Hello, " + name + "! (It is a web page.)";
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
