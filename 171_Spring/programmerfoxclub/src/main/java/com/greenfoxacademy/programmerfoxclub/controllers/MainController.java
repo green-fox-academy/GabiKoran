24c52984c87a2a724a47cb9a10d1b452e4383537
GabiKoran <korangabriella@gmail.com>
2020-02-24 11:29:58 +0100

    login page without optionals

--- 171_Spring/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ 171_Spring/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.programmerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,5 +11,15 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginGet() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@RequestParam String name) {
+        return "redirect:/?name=" + name;
     }
 }
---
+++ 171_Spring/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/services/FoxService.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.programmerfoxclub.services;
+
+import com.greenfoxacademy.programmerfoxclub.models.Fox;
+
+import java.util.ArrayList;
+
+public class FoxService {
+
+    private ArrayList<Fox> foxes;
+}
