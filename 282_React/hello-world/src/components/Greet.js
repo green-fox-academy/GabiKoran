c030f0edc2b0b67a0e0a27fb2d694d65adec6bb2
GabiKoran <korangabriella@gmail.com>
2020-05-06 01:01:47 +0200

    change Greet function to ES6 arrow syntax

--- 282_React/hello-world/src/components/Greet.js
+++ 282_React/hello-world/src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Gabi</h1>
-}
+// function Greet() {
+//     return <h1>Hello Gabi</h1>
+// }
+
+const Greet = () => <h1>Hello Gabi</h1>
 
 export default Greet
