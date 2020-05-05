d54bb9bad533c65c7b50036f1dee0d000e2517d4
GabiKoran <korangabriella@gmail.com>
2020-05-06 01:29:34 +0200

    without jsx - use createElement method

--- 282_React/hello-world/src/components/Hello.js
+++ 282_React/hello-world/src/components/Hello.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello Gabi</h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>Hello Gabi</h1>
+    //     </div>
+    // )
+    return React.createElement('div', null, 'Hello Gabi')
 }
 
 export default Hello
