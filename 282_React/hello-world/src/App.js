7378ac9ea67e9b655345139495a88f41df86c991
GabiKoran <korangabriella@gmail.com>
2020-05-06 02:16:35 +0200

    props

--- 282_React/hello-world/src/App.js
+++ 282_React/hello-world/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import Greet from './components/Greet'
+import Greet from './components/Greet'
 // import Welcome from './components/Welcome'
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet /> 
-      <Welcome />  */}
-      <Hello />
+      <Greet name="Bruce" heroName="Batman"></Greet>
+      <Greet name="Clark" heroName="Superman"/>
+      <Greet name="Diana" heroName="Wonder Woman"/>
+      <Greet />
+      {/* <Welcome /> */}
+      {/* <Hello /> */}
     </div>
   );
 }
