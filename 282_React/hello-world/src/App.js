184b00b763e0ca698e23055ee76a4014a9e05cd2
GabiKoran <korangabriella@gmail.com>
2020-05-06 13:01:06 +0200

    setStatus with input parameter (prevStat)

--- 282_React/hello-world/src/App.js
+++ 282_React/hello-world/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 // import Greet from './components/Greet'
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 // import Message from './components/Message'
-import Count from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Count />
+      <Counter />
       {/* <Message></Message> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>inner HTML text as children</p>
