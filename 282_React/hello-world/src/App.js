8165772d6c3644b6e0563639d26560edff2492c1
GabiKoran <korangabriella@gmail.com>
2020-05-11 11:39:46 +0200

    13 Event Handling

--- 282_React/hello-world/src/App.js
+++ 282_React/hello-world/src/App.js
@@ -2,15 +2,20 @@ import React from 'react'
 // import logo from './logo.svg';
 import './App.css'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 // import Greet from './components/Greet'
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 // import Message from './components/Message'
 
+
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FunctionClick />
+      <ClassClick />
+      {/* <Counter /> */}
       {/* <Message></Message> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>inner HTML text as children</p>
