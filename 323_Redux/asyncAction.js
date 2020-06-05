99ae7f9062c9b51df2b0b3bd033c4048b97ad829
GabiKoran <korangabriella@gmail.com>
2020-06-05 16:35:09 +0200

    async actions in a redux app

--- 323_Redux/asyncAction.js
+++ 323_Redux/asyncAction.js
@@ -1,5 +1,9 @@
 const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
 
 const initialState = {
   loading: false,
@@ -17,7 +21,7 @@ const fetchUsersRequest = () => {
   };
 };
 
-const fetchUsersSuccess = (user) => {
+const fetchUsersSuccess = (users) => {
   return {
     type: FETCH_USERS_SUCCESS,
     payload: users,
@@ -55,4 +59,24 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        // error.message is the error description
+        dispatch(fetchUserFailure(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
